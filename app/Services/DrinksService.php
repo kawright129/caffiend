98bf7192987e437fbb117bd88e215a641d9271ff
Kasey Wright <kawright129@gmail.com>
2021-08-02 10:43:53 -0400

    Adds return type declaration to DrinksService

--- app/Services/DrinksService.php
+++ app/Services/DrinksService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Repositories\Interfaces\DrinkRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
 
 class DrinksService
 {
@@ -10,7 +11,7 @@ class DrinksService
     public function __construct(protected DrinkRepositoryInterface $drinkRepository){ }
 
 
-    public function getDrinks(string $searchValue = "")
+    public function getDrinks(string $searchValue = ""): ?Collection
     {
         return $this->drinkRepository->getEnabledDrinks($searchValue);
     }
